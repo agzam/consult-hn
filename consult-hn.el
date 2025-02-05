6f955420fd6bc20ba04833f01d9dfa639d05a3bd
Ag Ibragimov <agzam.ibragimov@gmail.com>
2025-02-04 21:54:23 -0600

    simplified a bunch of things
    turns out it's easier to keep propertized strings instead of list of plists

--- consult-hn.el
+++ consult-hn.el
@@ -40,23 +40,13 @@
   :type 'string
   :group 'consult-hn)
 
-(defcustom consult-hn-preview-story-fn #'consult-hn-story-eww
+(defcustom consult-hn-preview-fn #'consult-hn-eww
   "Function pointer for previewing selected HN Story."
   :type 'function
   :group 'consult-hn)
 
-(defcustom consult-hn-preview-comment-fn #'consult-hn-comment-eww
-  "Function pointer for previewing selected comment."
-  :type 'function
-  :group 'consult-hn)
-
-(defcustom consult-hn-browse-story-fn #'consult-hn-story-eww
+(defcustom consult-hn-browse-fn #'consult-hn-eww
   "Function pointer for browsing selected HN Story."
-  :type 'function
-  :group 'consult-hn)
-
-(defcustom consult-hn-browse-comment-fn #'consult-hn-comment-eww
-  "Function pointer for browsing selected HN Comment."
   :type 'function
   :group 'consult-hn)
 
@@ -73,8 +63,8 @@
 (defvar consult-hn--history nil
   "History of queries for consult-hn.")
 
-(cl-defun consult-hn-story-eww (&key story-url hn-story-url author created-at hn-object-url num-comments points &allow-other-keys)
-  "Open hackernews story in eww buffer."
+(cl-defun consult-hn-eww (&key story-url title hn-story-url author created-at hn-object-url num-comments points comment &allow-other-keys)
+  "Open hackernews item in eww buffer."
   (cl-labels ((after-render-a (ofn status url &optional point buffer encode)
                 (unwind-protect
                     (progn
@@ -84,49 +74,26 @@
                         (goto-char (point-min))
                         (shr-insert-document
                          (with-temp-buffer
-                           (insert (format "<a href=\"%s\">%s</a>" hn-story-url hn-story-url))
-                           (insert "<br/>")
-                           (insert (format
-                                    (concat "<span> <a href=\"%s\">%s</a> | "
-                                            "<a href=\"%s\">%s</a> | "
-                                            "%s points | "
-                                            "%s comments |"
-                                            "</span>")
-                                    (concat "https://news.ycombinator.com/user?id=" author) author
-                                    hn-object-url (consult-hn--time-ago (ts-unix (ts-parse created-at)))
-                                    points num-comments))
+                           (insert "<div>")
+                           (insert (format "<div><a href=\"%s\">%s</a></div>" story-url title))
+                           (when comment
+                            (insert (format "<div><a href=\"%s\">%s</a></div>" hn-story-url hn-story-url)))
+                           (insert "<div><span>")
+                           (insert (format "<a href=\"%s\">%s</a>"
+                                           (concat "https://news.ycombinator.com/user?id=" author)
+                                           author))
+                           (insert (format " | <a href=\"%s\">%s</a>"
+                                           hn-object-url
+                                           (consult-hn--time-ago (ts-unix (ts-parse created-at)))))
+                           (when points (insert (format " | %s points" points)))
+                           (when num-comments (insert (format " | %s comments" num-comments)))
+                           (insert "</span></div>")
+                           (insert "</div>")
                            (insert "<hr/><br/><br/>")
                            (libxml-parse-html-region)))))
                  (advice-remove 'eww-render #'after-render-a))))
     (advice-add 'eww-render :around #'after-render-a)
     (eww (or story-url hn-story-url) :new-buffer)))
-
-(cl-defun consult-hn-comment-eww (&key story-url hn-story-url author created-at hn-object-url &allow-other-keys)
-  "Open hackernews comment in eww buffer."
-  (cl-labels ((after-render-a (ofn status url &optional point buffer encode)
-                (unwind-protect
-                    (progn
-                      (funcall ofn status url point buffer encode)
-                      (with-current-buffer buffer
-                        (read-only-mode -1)
-                        (goto-char (point-min))
-                        (shr-insert-document
-                         (with-temp-buffer
-                           (insert (format "<a href=\"%s\">%s</a>" story-url story-url))
-                           (insert "\n")
-                           (insert (format "<a href=\"%s\">%s</a>" hn-story-url hn-story-url))
-                           (insert "<br/>")
-                           (insert (format
-                                    (concat "<span> <a href=\"%s\">%s</a> | "
-                                            "<a href=\"%s\">%s</a></span>")
-                                    (concat "https://news.ycombinator.com/user?id=" author)
-                                    author hn-object-url
-                                    (consult-hn--time-ago (ts-unix (ts-parse created-at)))))
-                           (insert "<hr/><br/><br/>")
-                           (libxml-parse-html-region)))))
-                  (advice-remove 'eww-render #'after-render-a))))
-    (advice-add 'eww-render :around #'after-render-a)
-    (eww hn-object-url :new-buffer)))
 
 (defun consult-hn--fill-string (str &optional width justify)
   "Fills the STR string with WIDTH and JUSTIFY options."
@@ -136,18 +103,6 @@
       (insert str)
       (fill-region-as-paragraph (point-min) (point-max) (or justify 'left))
       (buffer-string))))
-
-(defun consult-hn--plist->prop-string (plist str-key)
-  "Convert PLIST to a propertized string.
-
-STR-KEY is a prop value of the PLIST that will be used for the string
-representation, all the rest of the keys will become properties of that
-string."
-  (apply #'propertize (plist-get plist str-key)
-         (cl-loop for (key value) on plist by #'cddr
-                  when value
-                  collect (intern (substring (symbol-name key) 1))
-                  and collect value)))
 
 (defun consult-hn--plist-keywordize (plist)
   "Keywordize keys in a PLIST."
@@ -247,17 +202,18 @@ timestamp value must be in utc timezone."
                                (object-url (format hn-base-url (gethash "objectID" x)))
                                (points (gethash "points" x))
                                (num-comments (gethash "num_comments" x)))
-                          (list
-                           :title (replace-regexp-in-string " +" " " title) ; titles shouldn't have two or more spaces
-                           :author author
-                           :comment comment-text
-                           :created-at created-at
-                           :story-url story-url
-                           :hn-story-url hn-story-url
-                           :hn-object-url object-url
-                           :ts ts
-                           :points points
-                           :num-comments num-comments)))))))
+                          (propertize
+                           (replace-regexp-in-string " +" " " title) ; titles shouldn't have two or more spaces
+                           'title title
+                           'author author
+                           'comment comment-text
+                           'created-at created-at
+                           'story-url story-url
+                           'hn-story-url hn-story-url
+                           'hn-object-url object-url
+                           'ts ts
+                           'points points
+                           'num-comments num-comments)))))))
         (setq all-rows (append all-rows rows))
         (setq nbPages (gethash "nbPages" result))
         (setq page (1+ (gethash "page" result)))
@@ -269,16 +225,15 @@ timestamp value must be in utc timezone."
     coll
     (seq-map
      (lambda (x)
-       (let* ((row (consult-hn--plist->prop-string x :title))
-              (row (truncate-string-to-width row 69 nil nil "..."))
+       (let* ((row (truncate-string-to-width x 69 nil nil "..."))
               (_ (add-text-properties 0 (length row) '(face bold) row)) ; title is bold
-              (author (plist-get x :author))
-              (created-at (propertize (plist-get x :created-at)
-                                      'invisible t))
-              (ago (consult-hn--time-ago (plist-get x :ts)))
-              ;; add comment to the row, but hide it
+              (author (get-text-property 0 'author x))
+              (ago (consult-hn--time-ago (get-text-property 0 'ts x)))
+              ;; add comment and created at to the row, but hide them
               ;; this is a trick to make the comments "filterable"
-              (comment (if-let* ((cmt (plist-get x :comment)))
+              (created-at (propertize (get-text-property 0 'created-at x)
+                                      'invisible t))
+              (comment (if-let* ((cmt (get-text-property 0 'comment x)))
                            (propertize cmt 'invisible t) "")))
          (format "%-75s   %-20s   %20s   %s   %s"
                  row author ago created-at comment))))))
@@ -301,8 +256,7 @@ timestamp value must be in utc timezone."
                           (string= created-at r-created-at)
                           (string-prefix-p (replace-regexp-in-string "^#\\|[.][.][.]$" "" title)
                                            (replace-regexp-in-string " +" " " r-title)))))))))
-      (or found
-          (display-warning 'consult-hn (format "couldn't match '%s' during lookup" parsed) :warning)))))
+      found)))
 
 (defun consult-hn (&optional initial)
   "Consult interface for searching on Hackernews."
@@ -314,22 +268,16 @@ timestamp value must be in utc timezone."
     (consult--async-transform #'consult-hn--async-transform))
    :lookup #'consult-hn--async-lookup
    :state (lambda (action cand)
-            (when-let* ((_ cand)
-                        (props (consult-hn--plist-keywordize
-                                (text-properties-at 0 cand))))
-              (cond
-               ((eq action 'preview)
-                (if (plist-get props :comment)
-                    (apply consult-hn-preview-comment-fn props)
-                  (apply consult-hn-preview-story-fn props)))
-               ((eq action 'return)
-                (if (plist-get props :comment)
-                    (apply consult-hn-browse-comment-fn props)
-                  (apply consult-hn-browse-story-fn props))))))
+            (when-let* ((hn-obj (consult-hn--plist-keywordize
+                                 (text-properties-at 0 (or cand "")))))
+              (when (member action '(preview return))
+                (apply consult-hn-preview-fn hn-obj))))
    :prompt "HN Search: "
    :sort nil
    :initial (or initial consult-hn-initial-input-string)
    :history '(:input consult-hn--history)
+   :require-match t
+   :category 'consult-hn-result
    :annotate (lambda (x)
                ;; comments shown as annotation
                (if-let* ((comment (get-text-property 0 'comment x))
@@ -338,6 +286,22 @@ timestamp value must be in utc timezone."
                                    (consult-hn--fill-string comment 120 'full))))
                    (format "\n%s" ann-txt)
                  ""))))
+
+;; (defun embark--consult-hn-refine (type target)
+;;   (pp (cons type (consult-hn--plist-keywordize
+;;                   (text-properties-at 0 target))))
+;;   (cons type (consult-hn--plist-keywordize
+;;               (text-properties-at 0 target))))
+
+;; (defun consult-hn--open (cand)
+;;   (edebug))
+
+;; (add-to-list
+;;  'embark-transformer-alist
+;;  '(consult-hn-result . embark--consult-hn-refine))
+
+;; (setf (alist-get 'consult-hn-result embark-default-action-overrides)
+;;         #'consult-hn--open)
 
 (provide 'consult-hn)
 ;;; consult-hn.el ends here
